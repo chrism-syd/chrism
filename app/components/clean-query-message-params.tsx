8f945c41fe17db16b662644172be96a588365a39
chrism-syd <sydsaddress@gmail.com>
2026-06-13 09:28:09 -0400

    Add global query message cleaner

---
+++ app/components/clean-query-message-params.tsx
@@ -0,0 +1,27 @@
+'use client'
+
+import { useEffect } from 'react'
+
+const MESSAGE_QUERY_KEYS = ['notice', 'error'] as const
+
+export default function CleanQueryMessageParams() {
+  useEffect(() => {
+    const currentUrl = new URL(window.location.href)
+    let changed = false
+
+    for (const key of MESSAGE_QUERY_KEYS) {
+      if (currentUrl.searchParams.has(key)) {
+        currentUrl.searchParams.delete(key)
+        changed = true
+      }
+    }
+
+    if (!changed) return
+
+    const nextSearch = currentUrl.searchParams.toString()
+    const nextUrl = `${currentUrl.pathname}${nextSearch ? `?${nextSearch}` : ''}${currentUrl.hash}`
+    window.history.replaceState(window.history.state, '', nextUrl)
+  }, [])
+
+  return null
+}
