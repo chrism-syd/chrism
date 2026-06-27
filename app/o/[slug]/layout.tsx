71b51b4ec996119335cc4f5bada1db0c96be2fad
chrism-syd <sydsaddress@gmail.com>
2026-06-26 23:47:58 -0400

    Remove layout-level contact enhancer

--- app/o/[slug]/layout.tsx
+++ app/o/[slug]/layout.tsx
@@ -2,13 +2,7 @@ import type { ReactNode } from 'react'
 import './public-page.css'
 import './public-gallery.css'
 import './public-contact-expander.css'
-import PublicContactFormExpander from './public-contact-form-expander'
 
 export default function PublicLocalOrganizationLayout({ children }: { children: ReactNode }) {
-  return (
-    <>
-      {children}
-      <PublicContactFormExpander />
-    </>
-  )
+  return children
 }
