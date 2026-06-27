ac208a819c98258c751b83f27e97241d5335e25b
chrism-syd <sydsaddress@gmail.com>
2026-06-26 21:57:23 -0400

    Import scoped public page stylesheet

--- app/o/[slug]/layout.tsx
+++ app/o/[slug]/layout.tsx
@@ -1,4 +1,5 @@
 import type { ReactNode } from 'react'
+import './public-page.css'
 import PublicContactFormExpander from './public-contact-form-expander'
 
 export default function PublicLocalOrganizationLayout({ children }: { children: ReactNode }) {
