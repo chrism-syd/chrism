f3b9f35293be2150d01635a80898e5afb288ee9f
chrism-syd <sydsaddress@gmail.com>
2026-06-26 22:24:05 -0400

    Import scoped public gallery stylesheet

--- app/o/[slug]/layout.tsx
+++ app/o/[slug]/layout.tsx
@@ -1,5 +1,6 @@
 import type { ReactNode } from 'react'
 import './public-page.css'
+import './public-gallery.css'
 import PublicContactFormExpander from './public-contact-form-expander'
 
 export default function PublicLocalOrganizationLayout({ children }: { children: ReactNode }) {
